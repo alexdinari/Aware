bec32bab780a19148d2ecd391ee824ec270bd74c
Your Name <alexdinari@roadhouseapps.com>
2015-05-27 17:57:56 -0700

    fixed view issues, rendering a blank screen on page load

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#index'
+  root to: 'application#index'
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       # /api/v1/co2trackers to retrieve all co2 levels globally
