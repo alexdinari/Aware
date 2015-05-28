42ecf10caf4af7552be64d9464bff8de44ee86da
Lawrence Tran <lawrencetran1@gmail.com>
2015-05-27 17:54:25 -0700

    Adds data and api endpoint for sea level

---
+++ app/models/sealevel_tracker.rb
@@ -0,0 +1,3 @@
+class SealevelTracker < ActiveRecord::Base
+  belongs_to :climate_tracker
+end
--- db/migrate/20150522211949_create_sealevel_trackers.rb
+++ db/migrate/20150522211949_create_sealevel_trackers.rb
@@ -1,9 +1,8 @@
-class CreateSeaTrackers < ActiveRecord::Migration
+class CreateSealevelTrackers < ActiveRecord::Migration
   def change
-    create_table :sea_trackers do |t|
+    create_table :sealevel_trackers do |t|
       t.integer :year
-      t.float :temp
-      t.float :rise_rate
+      t.float :sea_level
       t.references :climate_tracker, index: true, foreign_key: true
 
       t.timestamps null: false
