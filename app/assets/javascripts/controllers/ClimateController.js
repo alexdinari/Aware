515de340e8b0bbed709c9c4a94996c4a3096602e
Your Name <alexdinari@roadhouseapps.com>
2015-06-04 19:21:40 -0700

    resized air quality map, moving on to adding deaths

--- app/assets/javascripts/controllers/ClimateController.js
+++ app/assets/javascripts/controllers/ClimateController.js
@@ -557,8 +557,8 @@
 			}
 
 			function airquality(){
-				var margin = {top: 80, right: 80, bottom: 80, left: 80},
-    		width = 470 - margin.left - margin.right,
+				var margin = {top: 80, right: 80, bottom: 80, left: 40},
+    		width = 360 - margin.left - margin.right,
     		height = 250 - margin.top - margin.bottom;
 
 				var x = d3.scale.ordinal()
